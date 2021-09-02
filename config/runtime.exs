eb02a523dfb80bfb0f08e93b0de674a1ca9290fb
aswinmohanme <dev.aswinmohan@gmail.com>
2021-09-02 14:54:39 +0530

    Nop not going to deploy

--- config/runtime.exs
+++ config/runtime.exs
@@ -7,17 +7,6 @@ import Config
 # any compile-time configuration in here, as it won't be applied.
 # The block below contains prod specific runtime configuration.
 if config_env() == :prod do
-  secret_key_base =
-    System.get_env("SECRET_KEY_BASE") ||
-      raise """
-      environment variable SECRET_KEY_BASE is missing.
-      You can generate one by calling: mix phx.gen.secret
-      """
-
-  app_name =
-    System.get_env("FLY_APP_NAME") ||
-      raise "FLY_APP_NAME not available"
-
   database_url =
     System.get_env("DATABASE_URL") ||
       raise """
@@ -30,6 +19,17 @@ if config_env() == :prod do
     socket_options: [:inet6],
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+
+  secret_key_base =
+    System.get_env("SECRET_KEY_BASE") ||
+      raise """
+      environment variable SECRET_KEY_BASE is missing.
+      You can generate one by calling: mix phx.gen.secret
+      """
+
+  app_name =
+    System.get_env("FLY_APP_NAME") ||
+      raise "FLY_APP_NAME not available"
 
   config :indie_paper, IndiePaperWeb.Endpoint,
     http: [
