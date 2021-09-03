a7ad8b89b37c5525edafc85ad49e781bcf64a42b
aswinmohanme <dev.aswinmohan@gmail.com>
2021-09-03 12:16:44 +0530

    Setup Home Page Test

--- test/features/visit_home_page_test.exs
+++ test/features/visit_home_page_test.exs
@@ -1,9 +1,11 @@
 defmodule IndiePaperWeb.VisitHomePageTest do
   use IndiePaperWeb.FeatureCase, async: true
 
+  alias IndiePaperWeb.Pages.Page
+
   test "people can visit home page", %{session: session} do
     session
-    |> visit("/")
-    |> assert_has(data("test", "title", text: "IndiePaper"))
+    |> Page.Index.visit()
+    |> Page.Index.has_title()
   end
 end
