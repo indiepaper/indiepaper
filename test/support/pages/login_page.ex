b47926716840f9df226eeb0845973d20c2dbcc0b
aswinmohanme <dev.aswinmohan@gmail.com>
2021-09-03 20:38:36 +0530

    Login suprisingily works

--- test/support/pages/login_page.ex
+++ test/support/pages/login_page.ex
@@ -1,9 +1,15 @@
 defmodule IndiePaperWeb.Pages.LoginPage.Index do
   use IndiePaperWeb.PageHelpers
 
+  def visit(session) do
+    session
+    |> visit(Routes.author_session_path(@endpoint, :new))
+  end
+
   def login(session, email) do
     session
     |> fill_in(text_field("Email"), with: email)
-    |> fill_in(text_field("Email"), with: "longpassword123")
+    |> fill_in(text_field("Password"), with: "longpassword123")
+    |> click(button("Log in"))
   end
 end
