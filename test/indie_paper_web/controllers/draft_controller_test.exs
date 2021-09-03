67472d4fd4a9ddbe9fd6139706211b50bf88881f
aswinmohanme <dev.aswinmohan@gmail.com>
2021-09-03 16:12:29 +0530

    Setup Form Container

--- test/indie_paper_web/controllers/draft_controller_test.exs
+++ test/indie_paper_web/controllers/draft_controller_test.exs
@@ -2,5 +2,13 @@ defmodule IndiePaperWeb.DraftControllerTest do
   use IndiePaperWeb.ConnCase, async: true
 
   describe "create/2" do
+    test "displays error when invalid", %{conn: conn} do
+      result =
+        conn
+        |> post(Routes.draft_path(conn, :create), %{"draft" => %{}})
+        |> html_response(200)
+
+      assert result =~ "Oops"
+    end
   end
 end
