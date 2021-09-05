993d83b148d2f4611e0fabdd80b5814c6f9ce0b2
aswinmohanme <dev.aswinmohan@gmail.com>
2021-09-05 15:24:57 +0530

    Update esbuild to fix environment issue

--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule IndiePaper.MixProject do
       {:phoenix_live_view, "~> 0.16.0"},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.5"},
-      {:esbuild, git: "https://github.com/phoenixframework/esbuild", runtime: Mix.env() == :dev},
+      {:esbuild, "~> 0.3", runtime: Mix.env() == :dev},
       {:swoosh, "~> 1.3"},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
@@ -71,7 +71,7 @@ defmodule IndiePaper.MixProject do
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test --max-cases=2"],
       "assets.deploy": [
         "cmd --cd assets npm run deploy",
-        "esbuild --no-runtime-config default --minify",
+        "esbuild default --minify",
         "phx.digest"
       ]
     ]
