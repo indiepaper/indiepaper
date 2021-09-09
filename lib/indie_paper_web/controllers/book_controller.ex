ec539b3f245cffe552866ce8d1ff2da70a9ff2fd
aswinmohanme <dev.aswinmohan@gmail.com>
2021-09-09 22:04:43 +0530

    Render New Page for Books

--- lib/indie_paper_web/controllers/book_controller.ex
+++ lib/indie_paper_web/controllers/book_controller.ex
@@ -1,3 +1,10 @@
 defmodule IndiePaperWeb.BookController do
   use IndiePaperWeb, :controller
+
+  alias IndiePaper.Drafts
+
+  def new(conn, %{"draft_id" => draft_id}) do
+    draft = Drafts.get_draft!(draft_id)
+    render(conn, "new.html", draft: draft)
+  end
 end
--- test/support/pages/book_page.ex
+++ test/support/pages/book_page.ex
@@ -12,10 +12,21 @@ defmodule IndiePaperWeb.Pages.BookPage.New do
 
   def fill_form(session, attrs) do
     session
+    |> fill_in(text_field("Title"), with: attrs[:title])
+    |> fill_in(text_field("Short description"), with: attrs[:short_description])
+    |> fill_in(text_field("Long Description"), with: attrs[:long_description_html])
   end
 
   def click_publish(session) do
     session
     |> click(button("Publish Book"))
+  end
+end
+
+defmodule IndiePaperWeb.Pages.BookPage.Show do
+  use IndiePaperWeb.PageHelpers
+
+  def has_book_title?(session, _title) do
+    session
   end
 end
