881408e2b5b411feada10785d5d168c41145f824
aswinmohanme <dev.aswinmohan@gmail.com>
2021-09-19 22:59:46 +0530

    Correct Secret

--- config/runtime.exs
+++ config/runtime.exs
@@ -48,7 +48,7 @@ if config_env() == :prod do
   config :stripity_stripe,
     api_version: "2020-08-27",
     api_key: System.get_env("STRIPE_API_SECRET"),
-    connect_webhook_signing_secret: System.get_env("STRIPE_CONNECT_WEBHOOK_SECRET")
+    connect_webhook_signing_secret: System.get_env("STRIPE_CONNECT_WEBHOOK_SIGNING_SECRET")
 
   # ## Using releases
   #
