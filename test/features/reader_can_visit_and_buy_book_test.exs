96287476aed40d5d9a7c676a43490a571ce8f224
aswinmohanme <dev.aswinmohan@gmail.com>
2021-09-24 21:08:45 +0530

    Style show page

--- test/features/reader_can_visit_and_buy_book_test.exs
+++ test/features/reader_can_visit_and_buy_book_test.exs
@@ -6,8 +6,14 @@ defmodule IndiePaperWeb.Feature.ReaderCanVisitAndBuyBookTest do
   test "reader can visit book page and buy book", %{session: session} do
     book = insert(:book)
 
-    session
-    |> BookPage.Show.visit_page(book)
-    |> BookPage.Show.click_buy_button()
+    stripe_checkout_url =
+      session
+      |> BookPage.Show.visit_page(book)
+      |> BookPage.Show.click_buy_button()
+      |> current_url()
+
+    stripe_checkout_uri = URI.parse(stripe_checkout_url)
+
+    assert String.contains?(stripe_checkout_uri.host, "stripe.me")
   end
 end
