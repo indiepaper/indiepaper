4094b20d298f2c19e76bea9b39770df7dad860aa
aswinmohanme <dev.aswinmohan@gmail.com>
2021-09-25 20:09:06 +0530

    Clear migration of data dependency

--- priv/repo/migrations/20210925130332_add_stripe_checkout_session_id_to_orders.exs
+++ priv/repo/migrations/20210925130332_add_stripe_checkout_session_id_to_orders.exs
@@ -3,17 +3,7 @@ defmodule IndiePaper.Repo.Migrations.AddStripeCheckoutSessionIdToOrders do
 
   def change do
     alter table(:orders) do
-      add :stripe_checkout_session_id, :string
-    end
-
-    flush()
-
-    IndiePaper.Repo.update_all("orders",
-      set: [stripe_checkout_session_id: "dummy_checkout_session_id"]
-    )
-
-    alter table(:orders) do
-      modify :stripe_checkout_session_id, :string, null: false
+      add :stripe_checkout_session_id, :string, null: false
     end
   end
 end
