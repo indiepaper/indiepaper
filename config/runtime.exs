6fa8f5aeb174e4b581c9224e76a8a7a4586ace11
aswinmohanme <dev.aswinmohan@gmail.com>
2021-09-27 21:04:28 +0530

    Fix config name

--- config/runtime.exs
+++ config/runtime.exs
@@ -77,7 +77,7 @@ if config_env() == :prod do
     consumer_secret: System.get_env("UEBERAUTH_TWITTER_CONSUMER_SECRET")
 
   # Setup Swoosh with Postmark
-  config :sample, IndiePaper.Mailer,
+  config :indie_paper, IndiePaper.Mailer,
     adapter: Swoosh.Adapters.Postmark,
     api_key: System.get_env("SWOOSH_POSTMARK_API_KEY")
 
