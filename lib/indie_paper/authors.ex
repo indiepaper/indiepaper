794a87ef3f66045adf706a52f3a45695e94a4fca
aswinmohanme <dev.aswinmohan@gmail.com>
2021-09-27 11:36:54 +0530

    Confirm Socail Accounts on Sign in

--- lib/indie_paper/authors.ex
+++ lib/indie_paper/authors.ex
@@ -366,6 +366,7 @@ defmodule IndiePaper.Authors do
       _ ->
         %Author{}
         |> Author.registration_changeset(attrs)
+        |> Author.confirm_changeset()
         |> Repo.insert()
     end
   end
@@ -398,4 +399,7 @@ defmodule IndiePaper.Authors do
   def is_payment_connected?(author) do
     author.is_payment_connected
   end
+
+  def is_confirmed?(%{account_status: :confirmed}), do: true
+  def is_confirmed?(_), do: false
 end
