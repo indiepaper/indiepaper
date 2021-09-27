f7a71b40790a0e20416464451076851c12c3f5f1
aswinmohanme <dev.aswinmohan@gmail.com>
2021-09-27 18:56:51 +0530

    Update Status Plug to take list of statuses

--- lib/indie_paper_web/plugs/ensure_account_status_plug.ex
+++ lib/indie_paper_web/plugs/ensure_account_status_plug.ex
@@ -7,34 +7,41 @@ defmodule IndiePaperWeb.Plugs.EnsureAccountStatusPlug do
 
   def init(config), do: config
 
-  def call(conn, account_status) do
+  def call(conn, account_status_list) do
     conn
     |> IndiePaperWeb.AuthorAuth.fetch_current_author(%{})
-    |> has_account_status?(account_status)
-    |> maybe_halt(conn, account_status)
+    |> get_current_author()
+    |> has_account_status?(account_status_list)
+    |> maybe_halt(conn, account_status_list)
   end
 
+  defp get_current_author(conn), do: conn.assigns.current_author
+
+  defp has_account_status?(nil, _status_list), do: false
+
+  defp has_account_status?(author, status_list) when is_list(status_list),
+    do: Enum.any?(status_list, &has_account_status?(author, &1))
+
   defp has_account_status?(
-         %{assigns: %{current_author: %Author{account_status: account_status}}},
-         account_status
+         %Author{account_status: status},
+         status
        ),
        do: true
 
-  defp has_account_status?(
-         _conn,
-         _account_status
-       ),
-       do: false
+  defp has_account_status?(_conn, _status), do: false
 
   defp maybe_halt(true, conn, _), do: conn
 
-  defp maybe_halt(_any, conn, :payment_connected),
-    do:
-      redirect(
-        conn,
-        Routes.profile_stripe_connect_path(conn, :new),
-        "Connect with your Stripe Account to Publish and start recieving payments"
-      )
+  defp maybe_halt(_any, conn, status_list) do
+    cond do
+      Enum.find_value(status_list, fn status -> status == :payment_connected end) ->
+        redirect(
+          conn,
+          Routes.profile_stripe_connect_path(conn, :new),
+          "Connect with your Stripe Account to Publish and start recieving payments"
+        )
+    end
+  end
 
   defp redirect(conn, route, info_message) do
     conn
