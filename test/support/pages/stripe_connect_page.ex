e78cce0a1bc7643cf56f7aa3846f9c1c25601377
aswinmohanme <dev.aswinmohan@gmail.com>
2021-09-27 19:12:45 +0530

    Test Stripe Connect Redirect if not confirmed

--- test/support/pages/stripe_connect_page.ex
+++ test/support/pages/stripe_connect_page.ex
@@ -1,6 +1,11 @@
 defmodule IndiePaperWeb.Pages.StripeConnectPage.New do
   use IndiePaperWeb.PageHelpers
 
+  def visit_page(session) do
+    session
+    |> visit(Routes.profile_stripe_connect_path(@endpoint, :new))
+  end
+
   def select_country(session, country_code: country_code) do
     session
     |> fill_in(select("Country"), with: country_code)
