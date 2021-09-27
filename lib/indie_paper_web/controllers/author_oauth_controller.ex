f3ec9f966fc6ca0289d855489418e23cf461b489
aswinmohanme <dev.aswinmohan@gmail.com>
2021-09-27 08:48:02 +0530

    Set remember me cookie when signin in with Oauth

--- lib/indie_paper_web/controllers/author_oauth_controller.ex
+++ lib/indie_paper_web/controllers/author_oauth_controller.ex
@@ -33,7 +33,7 @@ defmodule IndiePaperWeb.AuthorOauthController do
 
     case Authors.fetch_or_create_author(author_params) do
       {:ok, author} ->
-        AuthorAuth.log_in_author(conn, author)
+        AuthorAuth.log_in_author(conn, author, %{"remember_me" => "true"})
 
       _ ->
         conn
