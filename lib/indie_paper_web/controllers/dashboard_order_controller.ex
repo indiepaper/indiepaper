e734fd90b4ae3b06d5fc202f71a782f5b7788ca9
aswinmohanme <dev.aswinmohan@gmail.com>
2021-09-28 17:26:13 +0530

    SHow success message on order page

--- lib/indie_paper_web/controllers/dashboard_order_controller.ex
+++ lib/indie_paper_web/controllers/dashboard_order_controller.ex
@@ -3,10 +3,20 @@ defmodule IndiePaperWeb.DashboardOrderController do
 
   alias IndiePaper.Orders
 
-  def index(%{assigns: %{current_author: customer}} = conn, _params) do
+  def index(%{assigns: %{current_author: customer}} = conn, params) do
     orders =
       Orders.list_orders(customer) |> Orders.with_assoc([:book, [line_items: [product: :assets]]])
 
-    render(conn, "index.html", orders: orders)
+    if(params["stripe_checkout_success"]) do
+      conn
+      |> put_flash(
+        :info,
+        "Hoorah!! Your purchase has been succesfully completed. You can find the order here."
+      )
+      |> render("index.html", orders: orders)
+    else
+      conn
+      |> render("index.html", orders: orders)
+    end
   end
 end
