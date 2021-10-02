49aaa99e2437284b42af147944d8b6c41863c3ee
aswinmohanme <dev.aswinmohan@gmail.com>
2021-10-02 23:49:17 +0530

    Setup database on DO

--- config/runtime.exs
+++ config/runtime.exs
@@ -15,8 +15,8 @@ if config_env() == :prod do
       """
 
   config :indie_paper, IndiePaper.Repo,
-    # ssl: true,
-    socket_options: [:inet6],
+    ssl: true,
+    # socket_options: [:inet6],
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
