408acdfb25af5ed6f0cfc787cecd7dd4c94ec850
aswinmohanme <dev.aswinmohan@gmail.com>
2021-11-11 21:17:36 +0530

    Finally managed to make this all work

--- lib/indie_paper/services/s3_handler.ex
+++ lib/indie_paper/services/s3_handler.ex
@@ -2,12 +2,11 @@ defmodule IndiePaper.Services.S3Handler do
   @bucket_name Application.get_env(:ex_aws, :bucket_name)
 
   def generate_presigned_url(key) do
-    query_params = [{"ContentType", "image/jpeg"}, {"ACL", "public-read"}]
-    presign_options = [virtual_host: false, query_params: query_params]
-
     %{fields: fields, url: url} =
       ExAws.Config.new(:s3)
-      |> ExAws.S3.presigned_post(@bucket_name, key, presign_options)
+      |> ExAws.S3.presigned_post(@bucket_name, key, acl: "public-read")
+
+    fields = Map.put(fields, "acl", "public-read")
 
     {:ok, url, fields}
   end
--- lib/indie_paper_web/views/book_view.ex
+++ lib/indie_paper_web/views/book_view.ex
@@ -4,4 +4,7 @@ defmodule IndiePaperWeb.BookView do
   def error_to_string(:too_large), do: "Too large"
   def error_to_string(:too_many_files), do: "You have selected too many files"
   def error_to_string(:not_accepted), do: "You have selected an unacceptable file type"
+
+  def error_to_string(:external_client_failure),
+    do: "An unexpected error occured while uploading the file."
 end
