815959a3374bb29f6248a891701439547cbfde64
aswinmohanme <dev.aswinmohan@gmail.com>
2021-11-11 20:11:37 +0530

    Set ACL as public and content type

--- lib/indie_paper/services/s3_handler.ex
+++ lib/indie_paper/services/s3_handler.ex
@@ -2,8 +2,12 @@ defmodule IndiePaper.Services.S3Handler do
   @bucket_name Application.get_env(:ex_aws, :bucket_name)
 
   def generate_presigned_url(key) do
+    query_params = [{"ContentType", "image/jpeg"}, {"ACL", "public-read"}]
+    presign_options = [virtual_host: false, query_params: query_params]
+
     %{fields: fields, url: url} =
-      ExAws.Config.new(:s3) |> ExAws.S3.presigned_post(@bucket_name, key)
+      ExAws.Config.new(:s3)
+      |> ExAws.S3.presigned_post(@bucket_name, key, presign_options)
 
     {:ok, url, fields}
   end
