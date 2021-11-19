daf5d030a01e8d70755b2001028ffc6913e105d1
aswinmohanme <dev.aswinmohan@gmail.com>
2021-11-19 21:38:04 +0530

    Disable instant.page because it's causing some issues

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,7 +25,7 @@ import "phoenix_html";
 import { Socket } from "phoenix";
 import { LiveSocket } from "phoenix_live_view";
 import topbar from "../vendor/topbar";
-import "instant.page";
+// import "instant.page";
 
 import Alpine from "alpinejs";
 window.Alpine = Alpine;
