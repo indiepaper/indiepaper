9e17b32081de6e1dd3bb3b5bff923264997f3b3c
aswinmohanme <dev.aswinmohan@gmail.com>
2021-11-20 11:43:11 +0530

    Test authentication on Profile LiveView

--- lib/indie_paper_web/live/settings_profile_live.ex
+++ lib/indie_paper_web/live/settings_profile_live.ex
@@ -1,3 +1,5 @@
 defmodule IndiePaperWeb.SettingsProfileLive do
   use IndiePaperWeb, :live_view
+
+  on_mount IndiePaperWeb.AuthorLiveAuth
 end
--- test/indie_paper_web/live/settings_profile_live_test.exs
+++ test/indie_paper_web/live/settings_profile_live_test.exs
@@ -1,2 +1,12 @@
 defmodule IndiePaperWeb.SettingsProfileLiveTest do
+  use IndiePaperWeb.ConnCase, async: true
+
+  import Phoenix.LiveViewTest
+
+  test "redirects to registration page when author is not present", %{conn: conn} do
+    {:error, {:redirect, %{to: registration_path}}} =
+      live(conn, Routes.settings_profile_path(conn, :edit))
+
+    assert registration_path == Routes.author_registration_path(conn, :new)
+  end
 end
