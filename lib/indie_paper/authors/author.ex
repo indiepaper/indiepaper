15a60dd83959cedcf6e1be7dcbcc72ab7d2b0281
aswinmohanme <dev.aswinmohan@gmail.com>
2021-11-20 13:15:46 +0530

    Only allow correctly formatted username

--- lib/indie_paper/authors/author.ex
+++ lib/indie_paper/authors/author.ex
@@ -164,6 +164,7 @@ defmodule IndiePaper.Authors.Author do
     |> cast(attrs, [:username, :first_name, :last_name])
     |> validate_required([:username, :first_name])
     |> validate_length(:username, min: 3, max: 32)
+    |> validate_format(:username, ~r/^[a-zA-Z0-9\_\-]+$/)
     |> validate_length(:first_name, max: 128)
     |> validate_length(:last_name, max: 128)
     |> unsafe_validate_unique(:username, IndiePaper.Repo)
