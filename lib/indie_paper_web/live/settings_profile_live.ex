6e7287d24e9a318ad2a3aa29ea1a1ebbbd0b7a8d
aswinmohanme <dev.aswinmohan@gmail.com>
2021-11-20 12:20:50 +0530

    Authors can update their settings

--- lib/indie_paper_web/live/settings_profile_live.ex
+++ lib/indie_paper_web/live/settings_profile_live.ex
@@ -10,4 +10,10 @@ defmodule IndiePaperWeb.SettingsProfileLive do
     changeset = AuthorProfile.change_profile(socket.assigns.current_author)
     {:ok, assign(socket, changeset: changeset)}
   end
+
+  @impl true
+  def handle_event("update_profile", %{"author" => author_params}, socket) do
+    {:ok, _author} = AuthorProfile.update_profile(socket.assigns.current_author, author_params)
+    {:noreply, socket |> redirect(to: Routes.dashboard_path(socket, :index))}
+  end
 end
--- test/features/person_can_change_profile_test.exs
+++ test/features/person_can_change_profile_test.exs
@@ -6,15 +6,25 @@ defmodule IndiePaperWeb.Feature.PersonCanChangeProfileTest do
   setup :register_and_log_in_author
 
   test "person can change their profile", %{conn: conn} do
-    %{username: username, first_name: first_name} = params_for(:author)
+    %{username: username, first_name: first_name, last_name: last_name} = params_for(:author)
+
     {:ok, view, _html} = live(conn, Routes.settings_profile_path(conn, :edit))
 
-    html =
+    {:ok, conn} =
       view
-      |> form("[data-test=profile-form]", %{username: username, first_name: first_name})
+      |> form("[data-test=profile-form]", %{
+        author: %{
+          username: username,
+          first_name: first_name,
+          last_name: last_name
+        }
+      })
       |> render_submit()
-      |> follow_redirect(to: Routes.dashboard_path(conn, :index))
+      |> follow_redirect(conn, Routes.dashboard_path(conn, :index))
+
+    html = conn |> html_response(200)
 
     assert html =~ first_name
+    assert html =~ last_name
   end
 end
