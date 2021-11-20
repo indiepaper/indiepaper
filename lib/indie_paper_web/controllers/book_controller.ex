aad595a82bb1a33ae7294e893bab33944c65f5fa
aswinmohanme <dev.aswinmohan@gmail.com>
2021-11-20 16:07:28 +0530

    Delete unneeded actions from book controller

--- lib/indie_paper_web/controllers/book_controller.ex
+++ lib/indie_paper_web/controllers/book_controller.ex
@@ -5,22 +5,6 @@ defmodule IndiePaperWeb.BookController do
 
   action_fallback IndiePaperWeb.FallbackController
 
-  def new(conn, _params) do
-    changeset = Books.change_book(%Books.Book{})
-    render(conn, "new.html", changeset: changeset)
-  end
-
-  def create(%{assigns: %{current_author: current_author}} = conn, %{"book" => book_params}) do
-    case Books.create_book_with_draft(current_author, book_params) do
-      {:ok, book} ->
-        book_with_draft = Books.with_assoc(book, :draft)
-        redirect(conn, to: Routes.draft_path(conn, :edit, book_with_draft.draft))
-
-      {:error, changeset} ->
-        render(conn, "new.html", changeset: changeset)
-    end
-  end
-
   def show(conn, %{"id" => book_id}) do
     book = Books.get_book!(book_id) |> Books.with_assoc(:author)
     render(conn, "show.html", book: book)
