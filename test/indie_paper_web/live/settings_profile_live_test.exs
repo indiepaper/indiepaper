7e6f0780143b9b2838279ccb07bdf60aa7c9aaeb
aswinmohanme <dev.aswinmohan@gmail.com>
2021-11-20 12:37:54 +0530

    Fix Setup interfering with Tests

--- test/indie_paper_web/live/settings_profile_live_test.exs
+++ test/indie_paper_web/live/settings_profile_live_test.exs
@@ -9,28 +9,30 @@ defmodule IndiePaperWeb.SettingsProfileLiveTest do
     assert registration_path == Routes.author_registration_path(conn, :new)
   end
 
-  setup :register_and_log_in_author
+  describe "authenticated person can" do
+    setup :register_and_log_in_author
 
-  test "show changeset error when data is invalid", %{conn: conn} do
-    insert(:author, username: "testusername")
-    {:ok, view, _html} = live(conn, settings_profile_path(conn))
+    test "show changeset error when data is invalid", %{conn: conn} do
+      insert(:author, username: "testusername")
+      {:ok, view, _html} = live(conn, settings_profile_path(conn))
 
-    view
-    |> form(profile_form(), %{author: %{username: "testusername"}})
-    |> render_submit()
-
-    assert render(view) =~ "has already been taken"
-  end
-
-  test "shows validation error when data is invalid", %{conn: conn} do
-    {:ok, view, _html} = live(conn, settings_profile_path(conn))
-
-    html =
       view
-      |> form(profile_form(), %{author: %{first_name: nil}})
-      |> render_change()
+      |> form(profile_form(), %{author: %{username: "testusername"}})
+      |> render_submit()
 
-    assert html =~ "can&#39;t be blank"
+      assert render(view) =~ "has already been taken"
+    end
+
+    test "shows validation error when data is invalid", %{conn: conn} do
+      {:ok, view, _html} = live(conn, settings_profile_path(conn))
+
+      html =
+        view
+        |> form(profile_form(), %{author: %{first_name: nil}})
+        |> render_change()
+
+      assert html =~ "can&#39;t be blank"
+    end
   end
 
   defp profile_form() do
