68071a21f189de141dec9fe2dbcdf84c27d66950
aswinmohanme <dev.aswinmohan@gmail.com>
2021-11-21 21:54:38 +0530

    Resize to Square Profile Pictures

--- lib/indie_paper/image_handler.ex
+++ lib/indie_paper/image_handler.ex
@@ -4,7 +4,13 @@ defmodule IndiePaper.ImageHandler do
   end
 
   def resize_to_square(image, dimension) do
-    image |> Mogrify.gravity("center") |> Mogrify.resize_to_limit("#{dimension}x#{dimension}")
+    image
+    |> Mogrify.custom("resize", "#{dimension}x#{dimension}>")
+    |> Mogrify.custom("gravity", "center")
+    |> Mogrify.custom("background", "white")
+    |> Mogrify.custom("alpha", "remove")
+    |> Mogrify.custom("alpha", "off")
+    |> Mogrify.custom("extent", "#{dimension}x#{dimension}")
   end
 
   def open(path), do: Mogrify.open(path)
