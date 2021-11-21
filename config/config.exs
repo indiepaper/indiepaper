fee3a59d08102a694d4005cbe5701d8a8deb9584
aswinmohanme <dev.aswinmohan@gmail.com>
2021-11-21 23:40:33 +0530

    Enable cdn for Spaces

--- config/config.exs
+++ config/config.exs
@@ -67,7 +67,7 @@ config :ex_aws,
 
 config :ex_aws, :s3,
   scheme: "https://",
-  host: "nyc3.digitaloceanspaces.com",
+  host: "nyc3.cdn.digitaloceanspaces.com",
   region: "nyc3"
 
 # Import environment specific config. This must remain at the bottom
