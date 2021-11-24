d5eb5f1ef62242382674588c8e2bcd368cac9973
aswinmohanme <dev.aswinmohan@gmail.com>
2021-11-24 21:20:13 +0530

    Setup Initial Modal Component

--- lib/indie_paper_web/live/modal_component.ex
+++ lib/indie_paper_web/live/modal_component.ex
@@ -12,9 +12,15 @@ defmodule IndiePaperWeb.ModalComponent do
       phx-key="escape"
       phx-target={@myself}
       phx-page-loading>
-      <div class="phx-modal-content">
-        <%= live_patch raw("&times;"), to: @return_to, class: "phx-modal-close" %>
-        <%= live_component @component, @opts %>
+      <div class="fixed z-50 opacity-100 shadow-xl rounded-b-none md:rounded-b-xl p-0 bg-white inset-x-0 bottom-0 md:inset-y-0 container-box overflow-y-scroll m-auto md:h-3/4 md:w-4/5 max-w-6xl rounded-xl">
+        <div class="w-full flex flex-row items-end justify-end p-4 border-b border-primary-border bg-background">
+        <%= live_patch to: @return_to, class: "border rounded-full border-primary-border bg-white p-2 hover:bg-primary hover:text-white" do %>
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+        <% end %>
+        </div>
+            <%= live_component @component, @opts %>
       </div>
     </div>
     """
