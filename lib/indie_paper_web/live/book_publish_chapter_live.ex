e6aa6c51bca4543e9987e13c0d94f73bdf5a9ea9
aswinmohanme <dev.aswinmohan@gmail.com>
2021-12-03 20:04:33 +0530

    Show checkmark when selected

--- lib/indie_paper_web/live/book_publish_chapter_live.ex
+++ lib/indie_paper_web/live/book_publish_chapter_live.ex
@@ -11,12 +11,10 @@ defmodule IndiePaperWeb.BookPublishChapterLive do
   @impl true
   def mount(%{"book_id" => book_id, "id" => chapter_id}, _, socket) do
     chapter = Chapters.get_chapter!(chapter_id)
-    changeset = Chapters.change_chapter(chapter)
     book = Books.get_book!(book_id)
     author = Books.get_author(book)
     membership_tiers = MembershipTiers.list_membership_tiers(author)
 
-    {:ok,
-     socket |> assign(changeset: changeset, chapter: chapter, membership_tiers: membership_tiers)}
+    {:ok, socket |> assign(chapter: chapter, membership_tiers: membership_tiers)}
   end
 end
