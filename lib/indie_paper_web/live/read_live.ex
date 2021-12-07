e2cb7fb1fa8fab2d74df8a53ead49e5e985f3c76
aswinmohanme <dev.aswinmohan@gmail.com>
2021-12-07 21:34:06 +0530

    Show list of chapters

--- lib/indie_paper_web/live/read_live.ex
+++ lib/indie_paper_web/live/read_live.ex
@@ -8,7 +8,8 @@ defmodule IndiePaperWeb.ReadLive do
   @impl true
   def mount(%{"book_id" => book_id}, _session, socket) do
     book = Books.get_book!(book_id)
+    published_chapters = Books.get_published_chapters(book)
 
-    {:ok, socket |> assign(book: book)}
+    {:ok, socket |> assign(book: book, published_chapters: published_chapters)}
   end
 end
--- test/indie_paper_web/live/read_live_test.exs
+++ test/indie_paper_web/live/read_live_test.exs
@@ -3,6 +3,8 @@ defmodule IndiePaperWeb.ReadLiveTest do
 
   import Phoenix.LiveViewTest
 
+  alias IndiePaper.Chapters
+
   test "show the title of the book", %{conn: conn} do
     reader = insert(:author)
     book = insert(:book)
@@ -10,5 +12,21 @@ defmodule IndiePaperWeb.ReadLiveTest do
     {:ok, _view, html} = live(conn, Routes.book_read_path(conn, :index, book))
 
     assert html =~ book.title
+  end
+
+  test "show published chapters of book", %{conn: conn} do
+    reader = insert(:author)
+    chapter1 = insert(:chapter, published_content_json: nil)
+    chapter2 = insert(:chapter, published_content_json: nil)
+
+    book =
+      insert(:book, publishing_type: :serial, draft: build(:draft, chapters: [chapter1, chapter2]))
+
+    conn = log_in_author(conn, reader)
+    {:ok, _published_chapter} = Chapters.publish_free_serial_chapter(chapter1)
+    {:ok, _view, html} = live(conn, Routes.book_read_path(conn, :index, book))
+
+    assert html =~ chapter1.title
+    refute html =~ chapter2.title
   end
 end
