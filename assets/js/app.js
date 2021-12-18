43ee39b7df54b1c6f8c2dc7d32d16968f2192eb6
aswinmohanme <dev.aswinmohan@gmail.com>
2021-12-18 14:44:09 +0530

    Mobile View for Reader

--- assets/js/app.js
+++ assets/js/app.js
@@ -191,7 +191,7 @@ window.addEventListener("phx:page-loading-stop", () => {
 liveSocket.connect();
 
 // expose liveSocket on window for web console debug logs and latency simulation:
-liveSocket.enableDebug();
+//liveSocket.enableDebug();
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket;
