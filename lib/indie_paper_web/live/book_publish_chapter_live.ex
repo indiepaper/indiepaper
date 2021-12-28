7583bdf6f530f4badc3bb538c737a98bc6b96762
aswinmohanme <dev.aswinmohan@gmail.com>
2021-12-28 10:59:43 +0530

    Author can choose whether Free or Paid

--- lib/indie_paper_web/live/book_publish_chapter_live.ex
+++ lib/indie_paper_web/live/book_publish_chapter_live.ex
@@ -8,20 +8,32 @@ defmodule IndiePaperWeb.BookPublishChapterLive do
   alias IndiePaper.BookPublisher
   alias IndiePaper.Products
   alias IndiePaper.Chapters
+  alias IndiePaper.ChapterProducts
   alias IndiePaper.PaymentHandler.MoneyHandler
 
   @impl true
   def mount(%{"book_slug" => book_slug, "id" => chapter_id}, _, socket) do
     chapter = Chapters.get_chapter!(chapter_id)
+    chapter_products = ChapterProducts.list_chapter_products(chapter)
     book = Books.get_book_from_slug!(book_slug) |> Books.with_assoc([:draft, :products])
 
+    free_product = %Products.Product{
+      id: "free",
+      title: "Free",
+      description: "Chapter can be read by anyone",
+      price: MoneyHandler.new(0)
+    }
+
+    selected_product =
+      if Enum.empty?(chapter_products) do
+        free_product
+      else
+        chapter_product = List.first(chapter_products)
+        Products.get_product!(chapter_product.product_id)
+      end
+
     products_with_free = [
-      %Products.Product{
-        id: "free",
-        title: "Free",
-        description: "Chapter can be read by anyone",
-        price: MoneyHandler.new(0)
-      }
+      free_product
       | book.products
     ]
 
@@ -30,7 +42,8 @@ defmodule IndiePaperWeb.BookPublishChapterLive do
      |> assign(
        chapter: chapter,
        products: products_with_free,
-       book: book
+       book: book,
+       selected_product: selected_product
      )}
   end
 
