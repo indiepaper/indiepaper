6ca9b4de2f41537d590438d6083f3d60de828522
aswinmohanme <dev.aswinmohan@gmail.com>
2022-01-05 10:24:04 +0530

    Setup Initial Dockerfile from Pheonix 1.6.6

--- config/runtime.exs
+++ config/runtime.exs
@@ -6,6 +6,12 @@ import Config
 # and secrets from environment variables or elsewhere. Do not define
 # any compile-time configuration in here, as it won't be applied.
 # The block below contains prod specific runtime configuration.
+
+# Start the phoenix server if environment is set and running in a release
+if System.get_env("PHX_SERVER") && System.get_env("RELEASE_NAME") do
+  config :indie_paper, IndiePaperWeb.Endpoint, server: true
+end
+
 if config_env() == :prod do
   database_url =
     System.get_env("DATABASE_URL") ||
@@ -14,12 +20,19 @@ if config_env() == :prod do
       For example: ecto://USER:PASS@HOST/DATABASE
       """
 
+  maybe_ipv6 = if System.get_env("ECTO_IPV6"), do: [:inet6], else: []
+
   config :indie_paper, IndiePaper.Repo,
     # ssl: true,
-    socket_options: [:inet6],
     url: database_url,
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+    socket_options: maybe_ipv6
 
+  # The secret key base is used to sign/encrypt cookies and other secrets.
+  # A default value is used in config/dev.exs and config/test.exs but you
+  # want to use a different value for prod and you most likely don't want
+  # to check this value into version control, so we use an environment
+  # variable instead.
   secret_key_base =
     System.get_env("SECRET_KEY_BASE") ||
       raise """
@@ -27,30 +40,20 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  app_name =
-    System.get_env("FLY_APP_NAME") ||
-      raise "FLY_APP_NAME not available"
-
-  indie_paper_deploy_domain =
-    System.get_env("INDIEPAPER_DEPLOY_DOMAIN") || raise "INDIEPAPER_DEPLOY_DOMAIN not available"
+  host = System.get_env("PHX_HOST") || "indiepaper.me"
+  port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :indie_paper, IndiePaperWeb.Endpoint,
-    server: true,
-    url: [host: indie_paper_deploy_domain, port: 443, scheme: "https"],
-    check_origin: [
-      "https://#{app_name}.fly.dev",
-      "https://*.#{indie_paper_deploy_domain}"
-    ],
+    url: [host: host, port: 443],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: String.to_integer(System.get_env("PORT") || "4000")
+      port: port
     ],
     secret_key_base: secret_key_base,
-    cache_static_manifest: "priv/static/cache_manifest.json"
 
   # Stripe Config
   config :stripity_stripe,
