2ee6780f8c77a65d6d7e90358f002570fe3fa1e1
aswinmohanme <dev.aswinmohan@gmail.com>
2022-01-05 16:03:28 +0530

    Remove MembershipTiers slowly and Steadily

--- test/indie_paper_web/live/author_page_live_test.exs
+++ test/indie_paper_web/live/author_page_live_test.exs
@@ -1,25 +1,3 @@
 defmodule IndiePaperWeb.AuthorPageLiveTest do
   use IndiePaperWeb.ConnCase, async: true
-
-  import Phoenix.LiveViewTest
-
-  alias IndiePaper.Authors
-
-  test "redirects to sign up page when not logged in", %{conn: conn} do
-    membership_tier = insert(:membership_tier)
-    {:ok, view, _html} = live(conn, Routes.author_page_path(conn, :show, membership_tier.author))
-
-    {:ok, conn} =
-      view
-      |> element("[data-test=subscribe-#{membership_tier.id}]")
-      |> render_click()
-      |> follow_redirect(
-        conn,
-        Routes.author_registration_path(conn, :new,
-          return_to: Routes.author_page_path(conn, :show, membership_tier.author)
-        )
-      )
-
-    assert html_response(conn, 200) =~ Authors.get_full_name(membership_tier.author)
-  end
 end
