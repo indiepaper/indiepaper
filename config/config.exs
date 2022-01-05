b1c3d3863e4100650e80934aabbeb84fea8b41dd
aswinmohanme <dev.aswinmohan@gmail.com>
2022-01-05 12:55:11 +0530

    Why isn't this working

--- config/config.exs
+++ config/config.exs
@@ -35,12 +35,11 @@ config :swoosh, :api_client, false
 config :esbuild,
   version: "0.14.0",
   default: [
-    args: ~w(
-      js/app.js js/draft_editor.js js/book_description_editor.js js/book_reader.js js/simple_tip_tap_html_editor.js
+    args:
+      ~w( js/app.js js/draft_editor.js js/book_description_editor.js js/book_reader.js js/simple_tip_tap_html_editor.js
       --chunk-names=chunks/[name]-[hash] --splitting --format=esm --bundle --target=es2017
-      --minify --outdir=../priv/static/assets
-      --external:/fonts/* --external:/images/*
-    ),
+      --minify --outdir=../priv/static/assets/js
+      --external:/fonts/* --external:/images/* ),
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
   ]
