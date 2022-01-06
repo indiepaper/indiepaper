ea31b5ef6d1f6cdee09000a21c49a940d0ea4646
aswinmohanme <dev.aswinmohan@gmail.com>
2022-01-06 11:48:39 +0530

    Set check origin to conn to match all subdomains

--- config/runtime.exs
+++ config/runtime.exs
@@ -45,6 +45,7 @@ if config_env() == :prod do
 
   config :indie_paper, IndiePaperWeb.Endpoint,
     url: [host: host, port: 443],
+    check_origin: :conn,
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
