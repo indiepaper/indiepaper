f247b32307c6e7df3cf68bdba0259151ab4f40e9
aswinmohanme <dev.aswinmohan@gmail.com>
2022-01-07 21:25:26 +0530

    Test webhook handler

--- test/indie_paper/webhook_handler_test.exs
+++ test/indie_paper/webhook_handler_test.exs
@@ -1,3 +1,18 @@
 defmodule IndiePaper.WebhookHandlerTest do
   use IndiePaper.DataCase, async: true
+
+  alias IndiePaper.WebhookHandler
+  alias IndiePaper.Orders
+  alias IndiePaper.ReaderBookSubscriptions
+
+  describe "checkout_session_completed/1" do
+    test "creates a reader book subscription" do
+      order = insert(:order, status: :payment_pending)
+
+      {:ok, updated_order} =
+        WebhookHandler.handle_checkout_session_completed(order.stripe_checkout_session_id)
+
+      assert Orders.is_payment_completed?(updated_order)
+    end
+  end
 end
