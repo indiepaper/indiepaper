ddeeb6b8dc6e079e5689b8fb1d76c3834c50ea4e
aswinmohanme <dev.aswinmohan@gmail.com>
2022-01-10 00:39:31 +0530

    Fix Customer id changed during search and replace

--- priv/repo/migrations/20220107153326_make_customer_as_reader.exs
+++ priv/repo/migrations/20220107153326_make_customer_as_reader.exs
@@ -2,6 +2,6 @@ defmodule IndiePaper.Repo.Migrations.MakeReaderAsReader do
   use Ecto.Migration
 
   def change do
-    rename table(:orders), :reader_id, to: :reader_id
+    rename table(:orders), :customer_id, to: :reader_id
   end
 end
