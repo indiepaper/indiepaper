28644ce154b2e2fdc5d79e9b7a92738e220a8e58
aswinmohanme <dev.aswinmohan@gmail.com>
2022-01-19 17:20:02 +0530

    Remove Dead Product Views

--- lib/indie_paper_web/views/product_view.ex
+++
@@ -1,9 +0,0 @@
-defmodule IndiePaperWeb.ProductView do
-  use IndiePaperWeb, :view
-
-  def comment(assigns) do
-    ~H"""
-    <%= raw("<!--") %><%= render_block(@inner_block) %><%= raw("-->") %>
-    """
-  end
-end
