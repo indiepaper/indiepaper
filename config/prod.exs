eb78bf15fc771e4957926e3caf294b9412e3351c
aswinmohanme <dev.aswinmohan@gmail.com>
2022-01-19 21:28:20 +0530

    Remove Force SSL from messing up URLS

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :indie_paper, IndiePaperWeb.Endpoint,
-  cache_static_manifest: "priv/static/cache_manifest.json",
-  force_ssl: [rewrite_on: [:x_forwarded_host, :x_forwarded_port, :x_forwarded_proto]]
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
