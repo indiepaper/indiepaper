e6dd9197e91c36953ddc1c7d5b88471c96795515
aswinmohanme <dev.aswinmohan@gmail.com>
2022-01-23 14:51:13 +0530

    Row is the better choice

--- lib/indie_paper_web/live/book_product_live/form_component.ex
+++ lib/indie_paper_web/live/book_product_live/form_component.ex
@@ -52,9 +52,9 @@ defmodule IndiePaperWeb.BookProductLive.FormComponent do
     assets = Books.get_assets(assigns.book)
 
     ~H"""
-    <div class="flex flex-col items-start space-y-4">
+    <div class="flex flex-row items-center space-x-4">
         <%= for asset <- assets do %>
-            <label class="secondary-button space-x-2 hover:cursor-pointer">
+            <label class="secondary-button space-x-2 hover:cursor-pointer hover:border-accent">
                 <input id={asset.id} type="checkbox" name="product[asset_ids][]" value={asset.id} checked={asset.id in existing_ids} class="rounded-md text-accent focus:outline-none focus:ring-accent" />
                 <span><%= asset.title %></span>
             </label>
